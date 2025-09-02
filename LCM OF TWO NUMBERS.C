144e8c199f6b705573e8b335d32cb43a71c6179c
pravalika32001 <pravallikasikhakolli238@gmail.com>
2025-09-02 11:18:52 +0530

    Add files via upload

---
+++ LCM OF TWO NUMBERS.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    int gcd_value = gcd(a, b);
+    return (a / gcd_value) * b;
+}
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("LCM of %d and %d is %d\n", a, b, lcm(a, b));
+    return 0;
+}
